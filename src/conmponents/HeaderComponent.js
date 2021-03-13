7a4d150f21e4be47526242a2f15e7593e0096f1e
Sayed Sadat <SadatSayed92@gmail.com>
2021-03-12 19:12:56 -0800

    React Router Dom Updates

--- src/conmponents/HeaderComponent.js
+++ src/conmponents/HeaderComponent.js
@@ -41,22 +41,22 @@ class Header extends Component {
                             <Nav navbar>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/home">
-                                        <i className="fa fa-home fa-lg" />Home
+                                        <i className="fa fa-home fa-lg" /> Home
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/directory">
-                                        <i className="fa fa-list fa-lg" />Directory
+                                        <i className="fa fa-list fa-lg" /> Directory
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/aboutus">
-                                        <i className="fa fa-info fa-lg" />About
+                                        <i className="fa fa-info fa-lg" /> About
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/contactus">
-                                        <i className="fa fa-address-card fa-lg" />Contact Us
+                                        <i className="fa fa-address-card fa-lg" /> Contact Us
                                     </NavLink>
                                 </NavItem>
                             </Nav>
