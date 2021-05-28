e447fd7a15e88e078dda3bab2797b91a5d641088
Sayed Sadat <SadatSayed92@gmail.com>
2021-05-27 17:23:22 -0700

    Comment Form

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+< !--The core Firebase JS SDK is always required and must be listed first-- >
+<script src="https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js"></script>
+
+<!--TODO: Add SDKs for Firebase products that you want to use
+https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.6.1/firebase-analytics.js"></script>
+
+<script>
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDX__m1CQ8_ZczvM9cPQQrH2s7LiXoXoLs",
+    authDomain: "sayedreactproject.firebaseapp.com",
+    projectId: "sayedreactproject",
+    storageBucket: "sayedreactproject.appspot.com",
+    messagingSenderId: "996891621433",
+    appId: "1:996891621433:web:9c026393b9afbe9b10b5e8",
+    measurementId: "G-SW6QS9DB9T"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
